b512d47379be4b510ecdd9b4c63263f23f585556
VidhyanshuMishra <vidhyanshumishra377@gmail.com>
2023-07-03 02:20:23 +0530

    added more project

--- src/newcomponents/WorkCardData.js
+++ src/newcomponents/WorkCardData.js
@@ -1,6 +1,11 @@
 import pro1 from "../usefullFiles/Food.png"
-import pro2 from "../usefullFiles/project2.png"
-import pro3 from "../usefullFiles/project3.png"
+import pro2 from "../usefullFiles/weather.png"
+import pro3 from "../usefullFiles/CartApp.png"
+import pro4 from "../usefullFiles/SpotifyClone.png"
+import pro5 from "../usefullFiles/StopWatch.png"
+import pro6 from "../usefullFiles/travellingsite.png"
+import pro7 from "../usefullFiles/random.png"
+import pro8 from "../usefullFiles/bootstrap.png"
 
 const projectCardData=[
     {
@@ -12,16 +17,51 @@ const projectCardData=[
     },
     {
         imgsrc: pro2,
-        title: "Dummy Data",
+        title: "My Weather App",
         text: "Note that the development build is not optimized, To create a production build, use npm run build.",
-        view: "https://en.wikipedia.org/wiki/URL",
+        view: "https://vidweatherapp.netlify.app",
         source: "https://github.com/Vidhyanshu97/bestfoods.git"
     },
     {
         imgsrc: pro3,
-        title: "Dummy Data",
+        title: "My Cart App",
         text: "Note that the development build is not optimized, To create a production build, use npm run build.",
-        view: "https://en.wikipedia.org/wiki/URL",
+        view: "https://vidcartapp.netlify.app",
+        source: "https://lnkd.in/dcMgfNf6"
+    },
+    {
+        imgsrc: pro4,
+        title: "Spotify Clone",
+        text: "Note that the development build is not optimized, To create a production build, use npm run build.",
+        view: "https://vidspotifyclone.netlify.app/",
+        source: "https://github.com/Vidhyanshu97/bestfoods.git"
+    },
+    {
+        imgsrc: pro5,
+        title: "Stop Watch",
+        text: "Note that the development build is not optimized, To create a production build, use npm run build.",
+        view: "https://vidstopwatch.netlify.app/",
+        source: "https://github.com/Vidhyanshu97/bestfoods.git"
+    },
+    {
+        imgsrc: pro6,
+        title: "Travel Site",
+        text: "Note that the development build is not optimized, To create a production build, use npm run build.",
+        view: "https://vidtravellingsite.netlify.app/",
+        source: "https://github.com/Vidhyanshu97/bestfoods.git"
+    },
+    {
+        imgsrc: pro7,
+        title: "Travel Site",
+        text: "Note that the development build is not optimized, To create a production build, use npm run build.",
+        view: "https://vidrandomly.netlify.app/",
+        source: "https://github.com/Vidhyanshu97/bestfoods.git"
+    },
+    {
+        imgsrc: pro8,
+        title: "Bootstrap Project",
+        text: "Note that the development build is not optimized, To create a production build, use npm run build.",
+        view: "https://vidbootstrapproject.netlify.app/",
         source: "https://github.com/Vidhyanshu97/bestfoods.git"
     },
 ]
